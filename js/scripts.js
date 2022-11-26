0aeb2c1d8fcc44e9367df3befe912a945d66d869
Miki Brut <martiejarque@gmail.com>
2022-11-26 14:50:45 +0100

    main page - recipe page START button

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,20 @@ window.onload = function () {
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
   const startPage = document.getElementById('start-page');
+  const startPageRecipe = document.getElementById('start-page-recipe');
   const startButton = document.getElementById('start');
+  const startGameButton = document.getElementById('start-game');
   
   startButton.onclick = function () {
     startPage.style = "display: none";
-    canvas.classList.remove('hidden');
-    const game = new Game(ctx);
-    game.start();
+    startPageRecipe.classList.remove('hidden');
   }
+
+
+startGameButton.onclick = function () {
+  startPageRecipe.style = "display: none";
+  canvas.classList.remove('hidden');
+  const game = new Game(ctx);
+  game.start();
+}
 }
